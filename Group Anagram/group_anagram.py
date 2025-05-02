81844bd6aa4cdc59cb28c2c5dfe85bd91a6dc004
nadun_hasalanka <nadun202327@gmail.com>
2025-05-02 09:32:48 +0530

    group anagram solved

--- Group Anagram/group_anagram.py
+++ Group Anagram/group_anagram.py
@@ -1,13 +1,22 @@
 mp = {}
 gpangram = ["ate","eat"]
-for word in gpangram:
-    newword = (''.join(sorted(word)))
+def groupAnagrams(self, gpangram):
+    for word in gpangram:
+        sortedWord = (''.join(sorted(word)))
 
-    if (mp.get(newword, "None") != "None"):
-        mp[newword].append(word)
-        print("hellp")
-    else:
-        mp[newword] = [word]
-        
-        
-print(mp)
+        if (mp.get(sortedWord, "None") != "None"):
+            mp[sortedWord].append(word)
+        else:
+            mp[sortedWord] = [word]
+    
+    return mp.values()
+
+print(groupAnagrams(mp, gpangram))
+
+
+# Idea is to group anagrams together with the same key value
+# and then return the values of the dictionary as a list of lists.
+# complexity ?
+      # Time complexity: O(n * k log k)
+      # k log k is the time complexity of sorting a string of length k.
+      # n is the number of strings in the input list.
